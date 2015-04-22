7a8c3eadf165e5215663e7ac7078303764a48079
rosyswapna <rosy.swapna@gmail.com>
2015-04-22 12:51:57 +0530

    customer cst number

--- sql/oriental_alterations.sql
+++ sql/oriental_alterations.sql
@@ -1,6 +1,7 @@
 
 --22 April 2015--
 ALTER TABLE `0_debtors_master`  ADD `tin_no` VARCHAR(50) NOT NULL AFTER `address`;
+ALTER TABLE `0_debtors_master`  ADD `cst_no` VARCHAR(50) NULL AFTER `tin_no`
 
 INSERT INTO `oriental`.`0_sys_prefs` (`name`, `category`, `type`, `length`, `value`) VALUES ('tin_no', 'setup.company', 'varchar', '50', NULL);
 
