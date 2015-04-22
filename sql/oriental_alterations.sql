b5e12f2b141cdbccec48afbb96e00a0b07877e8d
rosyswapna <rosy.swapna@gmail.com>
2015-04-22 12:03:38 +0530

    db alterations added

---
+++ sql/oriental_alterations.sql
@@ -0,0 +1,15 @@
+
+--22 April 2015--
+ALTER TABLE `0_debtors_master`  ADD `tin_no` VARCHAR(50) NOT NULL AFTER `address`;
+
+INSERT INTO `oriental`.`0_sys_prefs` (`name`, `category`, `type`, `length`, `value`) VALUES ('tin_no', 'setup.company', 'varchar', '50', NULL);
+
+--
+-- Table structure for table `0_trans_vehilces`
+--
+CREATE TABLE IF NOT EXISTS `0_trans_vehilces` (
+  `trans_no` int(11) unsigned NOT NULL DEFAULT '0',
+  `type` smallint(6) unsigned NOT NULL DEFAULT '0',
+  `vehicle_no` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`trans_no`,`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
